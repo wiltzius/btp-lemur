9550b9cc956d571bb8c37d9d1cfcac881a42df1d
Tom Wiltzius <tom.wiltzius@gmail.com>
2014-10-04 20:55:37 +0000

    squashed remaining build warnings

--- LemurAptana/LemurApp/models.py
+++ LemurAptana/LemurApp/models.py
@@ -30,12 +30,12 @@ class BannerMessage(models.Model):
 
 
 class FacilityManager(models.Manager):
-    def get_query_set(self):
+    def get_queryset(self):
         """We have complicated ordering requirements (all facilities alphabetically,
            followed by the "non-facility" facility) so this function returns the
            list we want in the order we want, as a normal queryset"""
         # get the normal queryset, then additionally select a true/false column `non-facility`, then order first by this then by the facility name
-        return super(FacilityManager, self).get_query_set() \
+        return super(FacilityManager, self).get_queryset() \
                     .extra(select={'non-facility': 'id=1'}) \
                     .extra(order_by=['non-facility', 'name'])
 
