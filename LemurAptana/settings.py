acff11c32074e94aec82bd66589748371145be98
Tom Wiltzius <tom.wiltzius@gmail.com>
2015-12-24 09:06:33 +0000

    fix ignored files

---
+++ LemurAptana/settings.py
@@ -0,0 +1,124 @@
+# Django settings for LemurAptana project.
+
+from settings_local import *
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+ADMINS = (
+    ('Tom Wiltzius', 'tom@cowville.net'),
+)
+
+MANAGERS = ADMINS
+
+# Local time zone for this installation. Choices can be found here:
+# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
+# although not all choices may be available on all operating systems.
+# On Unix systems, a value of None will cause Django to use the same
+# timezone as the operating system.
+# If running in a Windows environment this must be set to the same as your
+# system time zone.
+TIME_ZONE = 'America/Chicago'
+
+# Language code for this installation. All choices can be found here:
+# http://www.i18nguy.com/unicode/language-identifiers.html
+LANGUAGE_CODE = 'en-us'
+
+SITE_ID = 1
+
+# If you set this to False, Django will make some optimizations so as not
+# to load the internationalization machinery.
+USE_I18N = False
+
+# If you set this to False, Django will not format dates, numbers and
+# calendars according to the current locale
+USE_L10N = True
+
+# Absolute path to the directory that holds media.
+# Example: "/home/media/media.lawrence.com/"
+#MEDIA_ROOT = base_project_directory + 'media/'
+
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash if there is a path component (optional in other cases).
+# Examples: "http://media.lawrence.com", "http://example.com/media/"
+#MEDIA_URL = '/static/'
+
+# URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
+# trailing slash.
+# Examples: "http://foo.com/media/", "/media/".
+#ADMIN_MEDIA_PREFIX = '/media/'
+
+STATIC_ROOT = base_project_directory + 'static/'
+STATIC_URL = '/static/'
+
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = '***REMOVED***'
+
+# Initial data loading directory
+FIXTURE_DIRS = (
+   base_project_directory + 'fixtures/',
+)
+
+# List of callables that know how to import templates from various sources.
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+#     'django.template.loaders.eggs.Loader',
+)
+
+TEMPLATE_CONTEXT_PROCESSORS =(
+    "django.core.context_processors.debug",
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.media",
+    "django.contrib.messages.context_processors.messages",
+    "django.contrib.auth.context_processors.auth",
+    "django.core.context_processors.request",
+    "LemurAptana.LemurApp.context_processors.restricted_facilities",
+    "LemurAptana.LemurApp.context_processors.banner_message"
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+)
+
+ROOT_URLCONF = 'LemurAptana.urls'
+
+TEMPLATE_DIRS = (
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    '/home/books2pr/lemur/LemurAptana/LemurApp/templates',
+    )
+
+# Use the new test runner from Django 1.6
+TEST_RUNNER = 'django.test.runner.DiscoverRunner'
+
+# Makes sessions expire when the browser is closed rather than persisting
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True
+
+SESSION_SERIALIZER = 'django.contrib.sessions.serializers.PickleSerializer'
+
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.staticfiles',
+    'django.contrib.messages',
+    'LemurAptana.LemurApp',
+    # Uncomment the next line to enable the admin:
+    'django.contrib.admin',
+    # Uncomment the next line to enable admin documentation:
+    'django.contrib.admindocs',
+)
+
+
+## Extra settings for the Lemur project
+AWS_KEY = '***REMOVED***'
+AWS_SECRET_KEY = '***REMOVED***'
+
+AWS_ASSOCIATE_TAG = '***REMOVED***'
