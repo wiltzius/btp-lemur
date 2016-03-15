b9d2b47b8c268d12dce454bc77048be4df844210
Tom Wiltzius <wiltzius@chromium.org>
2016-03-14 22:13:36 -0700

    start using classnames for errors

--- assets/js/InmateSearchProxy.jsx
+++ assets/js/InmateSearchProxy.jsx
@@ -1,36 +1,53 @@
 import React from 'react';
+import classNames from 'classnames';
 
 export default class InmateSearchProxy extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      paroled_date: 'loading...',
-      projected_parole: 'loading...',
+      parole_single: 'loading...',
       parent_institution: 'loading...'
     }
+  }
+
+  oldParoledDate() {
+    if (this.state.parole_single) {
+      const today = new Date();
+      const parole_date = new Date(this.state.parole_single);
+      if (parole_date.getTime() < today.getTime()) {
+        return true;
+      }
+    }
+    return false;
   }
 
   componentDidMount() {
     $.get('/lemur/inmate_search_proxy/' + this.props.inmatePk, (results) => {
       console.log(results);
       this.setState({
-        paroled_date: results.paroled_date || '--',
-        projected_parole: results.projected_parole || '--',
-        parent_insitution: results.parent_institution || "unknown"
+        parole_single: results.parole_single || '--',
+        parent_institution: results.parent_institution || "unknown"
       });
     }, "json");
   }
 
   render() {
     console.log(this.props.inmatePk);
+    const paroleClasses = classNames({
+      'docLabel': true,
+      'error': this.oldParoledDate()
+    });
+
     return <ul className="inmateDOC">
-      <li><span className="resultLabel">Parole/release date:</span><span className="resultvalue">{this.state.paroled_date}</span>
+      <h4>Current DOC/FBOP Records</h4>
+      <li>
+        <span className={paroleClasses}>Parole/release date:</span>
+        <span className="docValue">{this.state.parole_single}</span>
       </li>
-      <li><span className="resultLabel">Proj. parole/release date:</span><span
-          className="resultvalue">{this.state.projected_parole}</span>
-      </li>
-      <li><span className="resultLabel">Parent institution:</span><span className="resultvalue">{this.state.parent_institution}</span>
+      <li>
+        <span className="docLabel">Parent institution:</span>
+        <span className="docValue">{this.state.parent_institution}</span>
       </li>
     </ul>
   }
