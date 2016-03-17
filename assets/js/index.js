2e175cc93ad5cfcc290520337ac987d6dfac10b0
Tom Wiltzius <wiltzius@chromium.org>
2016-03-16 21:09:03 -0700

    remove comment

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,11 +4,6 @@ import OrderReopenLink from './OrderReopenLink.jsx';
 
 // bootstrap the React app by attaching InmateSearchProxy instance to their placeholders in the html template
 const inmate_search_proxy_containers = document.querySelectorAll('.inmateSearchProxyContainer');
-//for(var i=0; i < containers.length; i++) {
-//  var el = containers[i];
-//  var inmate_pk = el.attributes["data-inmate-id"].value;
-//  ReactDOM.render(<InmateSearchProxy inmatePk={inmate_pk}/>, el);
-//}
 Array.from(inmate_search_proxy_containers).forEach(el => {
   const inmate_pk = el.attributes["data-inmate-id"].value;
   ReactDOM.render(<InmateSearchProxy inmatePk={inmate_pk}/>, el);
