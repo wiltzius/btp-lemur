c760b93ec911a1e7415df166e5bdc0d295d63087
Tom Wiltzius <tom.wiltzius@gmail.com>
2017-07-03 00:02:59 -0400

    lazily create the order age policy setting

--- LemurAptana/LemurApp/models/settings_store.py
+++ LemurAptana/LemurApp/models/settings_store.py
@@ -11,7 +11,13 @@ class LemurSettingsStore(models.Model):
 
   @classmethod
   def order_age_warning(cls):
-    return int(cls.objects.get(settingName__exact='order_age_policy').settingValue)
+    try:
+      obj = cls.objects.get(settingName__exact='order_age_policy')
+    except cls.DoesNotExist:
+      obj = cls(settingName='order_age_policy', settingValue='3')
+      obj.save()
+
+    return int(obj.settingValue)
 
   def __str__(self):
     return self.settingName + ": " + self.settingValue
