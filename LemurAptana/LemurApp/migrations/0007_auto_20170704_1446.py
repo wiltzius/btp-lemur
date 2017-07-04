288e043fca3bd0648281f2ee3eb3f13945e43982
Tom Wiltzius <tom.wiltzius@gmail.com>
2017-07-04 15:48:29 -0400

    add address column to facility

---
+++ LemurAptana/LemurApp/migrations/0007_auto_20170704_1446.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('LemurApp', '0006_auto_20170703_1217'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='facility',
+            name='address',
+            field=models.CharField(max_length=250, blank=True, default=""),
+        ),
+        migrations.AlterField(
+            model_name='book',
+            name='order',
+            field=models.ForeignKey(related_name='books', to='LemurApp.Order'),
+        ),
+        migrations.AlterField(
+            model_name='order',
+            name='inmate',
+            field=models.ForeignKey(verbose_name='Inmate', related_name='orders', to='LemurApp.Inmate'),
+        ),
+    ]
