78b3b9ff5db6f43b1405068d9c997e4e106bce93
Tom Wiltzius <tom.wiltzius@gmail.com>
2017-07-15 23:28:30 -0700

    experiment

---
+++ deploy.py
@@ -0,0 +1,16 @@
+# noinspection PyUnresolvedReferences
+from getpass import getpass
+
+from plumbum import SshMachine
+# from plumbum.cmd import git
+
+
+def main():
+  with SshMachine('cowville.net', user='tom', port=23) as remote:
+    with remote.cwd('/home/books2pr/lemur'):
+      print(remote['git']['pull']())
+      print(remote['./production_restart_il.sh']())
+
+
+if __name__ == '__main__':
+  main()
