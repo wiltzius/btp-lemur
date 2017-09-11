bc025437b7afe33d69dd5ea98d2b53617cbf03fc
Tom Wiltzius <tom.wiltzius@gmail.com>
2017-09-10 17:45:58 -0700

    add inmate CSV serializer for bulk data export

--- LemurAptana/LemurApp/api/__init__.py
+++ LemurAptana/LemurApp/api/__init__.py
@@ -2,7 +2,9 @@ from rest_framework import routers
 
 from LemurAptana.LemurApp.api.facility import FacilityViewSet
 from LemurAptana.LemurApp.api.inmate import InmateViewSet
+from LemurAptana.LemurApp.api.inmate_csv import InmateCSVViewSet
 
 router = routers.DefaultRouter()
 router.register(r'inmates', InmateViewSet)
+router.register(r'inmates_csv', InmateCSVViewSet)
 router.register(r'facilities', FacilityViewSet)
