a5a9ced13433c64ca6bb73469332475012ec03c5
Tom Wiltzius <tom.wiltzius@gmail.com>
2017-09-30 15:42:12 -0700

    fix up going to order detail page after order is sent

---
+++ assets/js/OrderDetail.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import coreapi from './coreapi';
+import OrderSummarySnippet from "./OrderSummarySnippet";
+
+export default class OrderDetail extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true,
+      order: null
+    };
+  }
+
+  componentDidMount() {
+    // parse the order ID out of the url, todo obviously want to use a router for this instead
+    const matches = /lemur\/order\/detail\/([0-9]+)/.exec(window.location.pathname);
+    const order_id = matches[1];
+    console.log('found order id', order_id);
+
+    coreapi.client.action(coreapi.schema, ['orders', 'read'], {id: order_id}).then(res => {
+      this.setState({order: res});
+      this.setState({loading: false});
+    }).catch(err => {
+      console.log(err)
+    });
+
+  }
+
+  orderSentSnippet() {
+    if (this.state.order.status === 'SENT') {
+      return <div>
+        <p><strong><span className="orderHeader">Order #{this.state.order.id}</span> marked as sent, please ensure it is
+          delivered to the packing station.</strong></p>
+        <p><a href={"/lemur/order/invoice/" + this.state.order.id} target="_blank">Print Invoice</a></p>
+      </div>
+    }
+  }
+
+  render() {
+    if (this.state.loading) {
+      return <div>Loading...</div>
+    }
+    else {
+      return <div>
+        <h3>Order details</h3>
+        {this.orderSentSnippet()}
+        <OrderSummarySnippet order={this.state.order}/>
+      </div>
+    }
+  }
+}
---
+++ assets/js/OrderSummarySnippet.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+export default class OrderSummarySnippet extends React.Component {
+
+  render() {
+    const order = this.props.order;
+    if (!order.books) {
+      return <p><strong>There are no books in this order. <a href="">Add some</a> before sending it out.</strong></p>
+    }
+    else {
+      return <div>
+        <div id="sendReady">
+          <h4>Name and Address:</h4>
+          <p>{order.inmate.last_name}, {order.inmate.first_name}<br/>
+            {order.inmate.address}
+          </p>
+          <h4>Books:</h4>
+          <ul className="sendItems">
+            {_.map(order.books, book => {
+              return <li key={book.id}>
+                <span className="resName">{book.title}</span>{book.author ? <span>by {book.author}</span> : ''}
+              </li>
+            })}
+          </ul>
+        </div>
+      </div>
+    }
+  }
+}
