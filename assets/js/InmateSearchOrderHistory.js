98c23ec4738fd2063480621423be0f6b508681ce
Tom Wiltzius <tom.wiltzius@gmail.com>
2017-11-08 22:35:30 -0800

    show/hide order history

--- assets/js/InmateSearchOrderHistory.js
+++ assets/js/InmateSearchOrderHistory.js
@@ -10,7 +10,8 @@ export default class InmateSearchOrderHistory extends React.Component {
     super(props);
     this.state = {
       results: [],
-      loading: true
+      loading: true,
+      hidden: true
     }
   }
 
@@ -22,30 +23,41 @@ export default class InmateSearchOrderHistory extends React.Component {
     });
   }
 
+  toggleHidden() {
+    this.setState((prevState, props) => {
+      return {hidden: !prevState.hidden}
+    });
+  }
+
   render() {
     return <li>
-      <a>History</a>
-      <ul className="historyList" style={{display: 'block'}}>
-        <If condition={this.state.loading === false}>
-          <For each="order" of={this.state.results}>
-            <li key={order.id}>
-              {/* TODO restore show/hide of order here */}
-              <a>Order #{order.id}</a>, (<OrderReopenLink orderPk={order.id}/>)
-              opened {dateFormat(order.date_opened)}
-              <If condition={order.status === 'SENT'}>
-                , closed {dateFormat(order.date_closed)}
-                <If condition={order.sender}> by {order.sender}</If>
-              </If>
-              {/* todo show/hide */}
-              <ul className="orderlist" id={"orderList" + order.id} style={{display: 'block'}}>
-                <For each="book" of={order.books}>
-                  <li key={book.id}>{book.title}</li>
-                </For>
-              </ul>
-            </li>
-          </For>
-        </If>
-      </ul>
+      <a onClick={this.toggleHidden.bind(this)}>History</a>
+      <If condition={this.state.hidden}>
+        <ul className="historyList">
+          <If condition={this.state.loading === false}>
+            <For each="order"
+                 of={this.state.results}>
+              <li key={order.id}>
+                <a>Order #{order.id}</a>, (<OrderReopenLink orderPk={order.id}/>)
+                opened {dateFormat(order.date_opened)}
+                <If condition={order.status === 'SENT'}>
+                  , closed {dateFormat(order.date_closed)}
+                  <If condition={order.sender}> by {order.sender}</If>
+                </If>
+                {/* todo show/hide */}
+                <ul className="orderlist"
+                    id={"orderList" + order.id}
+                    style={{display: 'block'}}>
+                  <For each="book"
+                       of={order.books}>
+                    <li key={book.id}>{book.title}</li>
+                  </For>
+                </ul>
+              </li>
+            </For>
+          </If>
+        </ul>
+      </If>
     </li>
   }
 
