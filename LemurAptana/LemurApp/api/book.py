642b6ff276b597d17a203904a5637159b7f74c28
Tom Wiltzius <tom@welkinhealth.com>
2018-07-06 11:40:10 -0400

    add CSV exports for all datatypes

--- LemurAptana/LemurApp/api/book.py
+++ LemurAptana/LemurApp/api/book.py
@@ -1,9 +1,14 @@
 from rest_framework import serializers, viewsets
 
-from LemurAptana.LemurApp.models import Order, Book
+from LemurAptana.LemurApp.models import Book
 
 
 class BookSerializer(serializers.ModelSerializer):
   class Meta:
     model = Book
     fields = '__all__'
+
+
+class BookViewSet(viewsets.ModelViewSet):
+  queryset = Book.objects.all()
+  serializer_class = BookSerializer
